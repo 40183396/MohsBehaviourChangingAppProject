88cebc0a7954028e5c8273245928466dffcc650a
40183396 <40183396@live.napier.ac.uk>
2018-03-16 01:32:01 +0000

    Finished Account Settings Page

--- app/src/main/java/com/napier/mohs/instagramclone/Profile/AccountSettingsActivity.java
+++ app/src/main/java/com/napier/mohs/instagramclone/Profile/AccountSettingsActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.napier.mohs.instagramclone.R;
@@ -21,11 +23,26 @@ public class AccountSettingsActivity extends AppCompatActivity{
 
     private static final String TAG = "AccountSettingsActivity";
 
+    private Context mContext;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
+        mContext = AccountSettingsActivity.this;
         Log.d(TAG, "onCreate: started account settings activity");
+
+        setupSettingsList();
+
+        // setup back arrow for navigating back to "Profile Activity"
+        ImageView backArrow = (ImageView) findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: navigating back to profile page");
+                finish();
+            }
+        });
     }
 
     private void setupSettingsList(){
@@ -33,7 +50,10 @@ public class AccountSettingsActivity extends AppCompatActivity{
         ListView listview = (ListView) findViewById(R.id.listviewAccountSettings);
 
         ArrayList<String> options = new ArrayList<>();
-        options.add("Edit Profile");
-        
+        options.add(getString(R.string.edit_profile));
+        options.add(getString(R.string.sign_out));
+
+        ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
+        listview.setAdapter(adapter);
     }
 }
