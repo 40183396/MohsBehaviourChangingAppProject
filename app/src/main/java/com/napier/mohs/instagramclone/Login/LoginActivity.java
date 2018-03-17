65c85ed00ad4d5cef2eabb7c85cea9505b43a047
40183396 <40183396@live.napier.ac.uk>
2018-03-17 17:21:45 +0000

    Done Register Page

---
+++ app/src/main/java/com/napier/mohs/instagramclone/Login/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.napier.mohs.instagramclone.Login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.napier.mohs.instagramclone.R;
+
+/**
+ * Created by Mohs on 17/03/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate: started login");
+    }
+}
