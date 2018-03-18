d8b1daad58a9bfd6bf62055b2627890b3e75141b
40183396 <40183396@live.napier.ac.uk>
2018-03-18 23:29:13 +0000

    Next Activity Recieves Extra And Displays Image From Incoming Image URL Intent

--- app/src/main/java/com/napier/mohs/instagramclone/Share/NextActivity.java
+++ app/src/main/java/com/napier/mohs/instagramclone/Share/NextActivity.java
@@ -1,11 +1,25 @@
 package com.napier.mohs.instagramclone.Share;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.napier.mohs.instagramclone.R;
+import com.napier.mohs.instagramclone.Utils.FirebaseMethods;
+import com.napier.mohs.instagramclone.Utils.UniversalImageLoader;
 
 /**
  * Created by Mohs on 18/03/2018.
@@ -14,11 +28,107 @@ import com.napier.mohs.instagramclone.R;
 public class NextActivity extends AppCompatActivity{
     private static final String TAG = "NextActivity";
 
+    // Firebase Stuff
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myDBRefFirebase;
+    private FirebaseMethods mFirebaseMethods;
+
+    private String mAppend = "file:/";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
+        setupFirebaseAuth();
+        imageSet();
+
         Log.d(TAG, "onCreate: recieved selected image: " + getIntent().getStringExtra(getString(R.string.image_selected)));
+
+        // Button to close gallery
+        ImageView back = (ImageView) findViewById(R.id.imageNextBack);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: closing next activity");
+                finish();
+            }
+        });
+
+        // goes to share activty
+        TextView share = (TextView) findViewById(R.id.textviewNextShare);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: sharing image to firebase to db");
+                // uploads image to fb db
+            }
+        });
+    }
+
+
+    // when activity starts automatically sets image incomming image url of intent
+    private void imageSet(){
+        Intent intent = getIntent();
+        ImageView img = (ImageView) findViewById(R.id.imageNextShare);
+        // static call to universal image loader
+        UniversalImageLoader.setImage(intent.getStringExtra(getString(R.string.image_selected)), img, null, mAppend);
+        // do not need to check for null values as universal image loader can handle this
+    }
+
+
+
+    //------------------------FIRESBASE STUFF------------
+    // Method to check if a user is signed in app
+
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: firbase auth is being setup");
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myDBRefFirebase = mFirebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+                if(user != null){
+                    Log.d(TAG, "onAuthStateChanged: user signed in " + user);
+                } else {
+                    Log.d(TAG, "onAuthStateChanged: user signed out");
+                }
+            }
+        };
+
+        // allows to get datasnapshot and allows to read or write to db
+        myDBRefFirebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        FirebaseUser user = mAuth.getCurrentUser();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
